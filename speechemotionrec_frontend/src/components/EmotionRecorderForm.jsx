73d63e678d8cb568938e6c35a981259d89d71980
Akshay Kankal <akshay@Akshays-Mac-mini.local>
2024-09-09 19:32:47 +0530

    local

--- speechemotionrec_frontend/src/components/EmotionRecorderForm.jsx
+++ speechemotionrec_frontend/src/components/EmotionRecorderForm.jsx
@@ -110,7 +110,7 @@ const EmotionRecorderForm = () => {
       }
 
       
-      const response = await axios.post('http://localhost:10000/predict', formData, {
+      const response = await axios.post('http://127.0.0.1:10000/predict', formData, {
         headers: { 'Content-Type': 'multipart/form-data' }
       });
 
